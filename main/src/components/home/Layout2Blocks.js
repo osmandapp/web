3d7079a727e084754fcae2f8878627224d2b1d4c
Victor Shcherb <victor.shcherb@gmail.com>
2022-04-06 19:55:41 +0200

    Add poll api to service

--- main/src/components/home/Layout2Blocks.js
+++ main/src/components/home/Layout2Blocks.js
@@ -2,22 +2,22 @@ import React from 'react';
 
 export default function Layout2Blocks({ children }) {
     return (
-    <div className="container">
-        <div className="row padding-vert--md">
-            <div className="col col--1" />
-            
-            <div className="col col--4 padding-vert--md padding-horiz--lg" align="center">
-                {children[0]}
-            </div>
+        <div className="container">
+            <div className="row padding-vert--md">
+                <div className="col col--1" />
 
-            <div className="col col--2" />
+                <div className="col col--4 padding-vert--md padding-horiz--md" align="center">
+                    {children[0]}
+                </div>
 
-            <div className="col col--4 padding-vert--md padding-horiz--lg" align="center">
-                {children[1]}
+                <div className="col col--2"/>
+
+                <div className="col col--4 padding-vert--md padding-horiz--md" align="center">
+                    {children[1]}
+                </div>
+
+                <div className="col col--1" />
             </div>
-            
-            <div className="col col--1" />
         </div>
-    </div>
     );
 }
--- main/src/components/home/Twitter.js
+++ main/src/components/home/Twitter.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { TwitterTimelineEmbed } from 'react-twitter-embed';
 import boxStyles from './Box.module.css';
+import clsx from 'clsx';
 
 export default function Twitter() {
     // var fjs = document.getElementsByTagName(s)[0];
@@ -15,7 +16,7 @@ export default function Twitter() {
     //     <a class="twitter-timeline" href="https://twitter.com/osmandapp"
     //         data-widget-id="598236050113372160" height="502">Tweets by @osmandapp</a>
     // );
-    return <div className={boxStyles.box}>
+    return <div className={clsx(boxStyles.box, 'shadow--md')}>
         <TwitterTimelineEmbed
         sourceType="profile"
         screenName="osmandapp"
