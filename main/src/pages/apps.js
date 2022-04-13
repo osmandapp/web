19b66079bd108177500b84b12dde1043dd71714b
Victor Shcherb <victor.shcherb@gmail.com>
2022-04-13 22:53:00 +0200

    Add static pages

--- main/src/pages/apps.js
+++ main/src/pages/apps.js
@@ -1,16 +1,10 @@
 import React from 'react';
 import clsx from 'clsx';
 import Layout from '@theme/Layout';
-import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import styles from './index.module.css';
 
 import AllStores from '../components/buttons/AllStores.mdx';
-import Features from '../components/home/Features';
-import Poll from '../components/home/Poll';
-import Layout2Blocks from '../components/home/Layout2Blocks';
-import Twitter from '../components/home/Twitter';
-import Screenshots from '../components/home/Screenshots';
 function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
   return (
@@ -18,14 +12,6 @@ function HomepageHeader() {
       <div className="container">
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div >
-          <Link className="button button--secondary button--lg"
-            to="/docs/intro">Features — 🗺️ 🧭 ⛰️</Link>
-          {
-            // <Link className="button button--secondary button--lg margin-left--lg margin-top--md"
-            //  to="https://maptile.osmand.net/map">OsmAnd World Map</Link>
-          }
-        </div>
         <div className="margin-vert--lg">
           <AllStores />
         </div>
@@ -41,15 +27,6 @@ export default function Home() {
       title={`${siteConfig.title}`}
       description="Offline Mobile Maps &amp; Navigation">
       <HomepageHeader />
-      <main>
-        <Screenshots />
-        <Features />
-        <Layout2Blocks names={['Twitter', 'Poll']}>
-          <Twitter />
-          <Poll />
-        </Layout2Blocks>
-        
-      </main>
     </Layout>
   );
 }
