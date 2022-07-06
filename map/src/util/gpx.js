00e3f1b5829b35c1294ed2f0bdc88959b54d04dc
alisa911 <alisa.91150@gmail.com>
2022-07-06 21:33:51 +0300

    fix background

--- map/src/util/gpx.js
+++ map/src/util/gpx.js
@@ -635,7 +635,7 @@ L.GPX = L.FeatureGroup.extend({
             }
         } else {
             svg = `<svg class="background" viewBox="0 0 48 48" xmlns="http://www.w3.org/2000/svg">
-                                      <circle cx="24" cy="24" r="12" fill="#eecc22"/>
+                         <circle cx="24" cy="24" r="12" fill="${color}"/>
                         </svg>`
         }
         return svg;
