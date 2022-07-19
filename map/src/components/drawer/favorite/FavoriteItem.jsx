fb20d4e9bbe434201777eeab72c2ab2d4033d6ac
alisa911 <alisa.91150@gmail.com>
2022-07-19 11:10:14 +0300

    fix styles

--- map/src/components/drawer/favorite/FavoriteItem.jsx
+++ map/src/components/drawer/favorite/FavoriteItem.jsx
@@ -1,4 +1,4 @@
-import {ListItemText, MenuItem} from "@mui/material";
+import {ListItemText, MenuItem, Tooltip, Typography} from "@mui/material";
 import React, {useContext} from "react";
 import AppContext from "../../../context/AppContext";
 import {makeStyles} from "@material-ui/core/styles";
@@ -7,8 +7,8 @@ import {makeStyles} from "@material-ui/core/styles";
 const useStyles = makeStyles({
     icon: {
         "& .icon": {
-            top: '22px',
-           left: '30px',
+            top: '14px',
+            left: '30px',
         },
         "& .background": {
             left: '0px',
@@ -16,13 +16,21 @@ const useStyles = makeStyles({
             filter: "drop-shadow(0 0 0 gray)"
         }
     },
-    text: {
-        textOverflow: "clip !important",
-        wordWrap: 'normal !important',
+    item: {
         '& li': {
-            whiteSpace: "normal"
+            minHeight: '50px !important',
+            maxHeight: '50px !important',
         }
     },
+    text: {
+        '& p': {
+            whiteSpace: "nowrap",
+            overflow: "hidden",
+            textOverflow: "ellipsis",
+            width: "100%",
+            paddingRight: "20px"
+        }
+    }
 })
 
 export default function FavoriteItem({index, marker}) {
@@ -40,10 +48,17 @@ export default function FavoriteItem({index, marker}) {
     }
 
 
-    return <div className={classes.text}>
-        <MenuItem key={'marker' + index} sx={{ml: -1}} divider onClick={() => addFavoriteToMap(marker)}>
-            <div className={classes.icon} dangerouslySetInnerHTML={{__html: marker.options.icon.options.html + ''}}/>
-            <ListItemText> {marker.options.title} </ListItemText>
-        </MenuItem>
-        </div>
+    return <div className={classes.item}>
+        <Tooltip title={marker.options.title} arrow>
+            <MenuItem key={'marker' + index} sx={{ml: 2}} divider onClick={() => addFavoriteToMap(marker)}>
+                <div className={classes.icon}
+                     dangerouslySetInnerHTML={{__html: marker.options.icon.options.html + ''}}/>
+                <ListItemText className={classes.text}>
+                    <Typography variant="inherit">
+                        {marker.options.title}
+                    </Typography>
+                </ListItemText>
+            </MenuItem>
+        </Tooltip>
+    </div>
 }
