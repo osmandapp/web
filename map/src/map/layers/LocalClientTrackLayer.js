32c5a53978678acedb3c2355efad83fc7d90c366
alisa911 <alisa.91150@gmail.com>
2022-08-04 23:23:44 +0300

    refactoring

--- map/src/map/layers/LocalClientTrackLayer.js
+++ map/src/map/layers/LocalClientTrackLayer.js
@@ -9,7 +9,7 @@ export default function LocalClientTrackLayer() {
     const ctx = useContext(AppContext);
     const map = useMap();
 
-    const [layers, setLayers] = useState([]);
+    const [layers, setLayers] = useState({});
     const markerOptions = {
         startIcon: MarkerIcon({bg: 'blue'}),
         endIcon: MarkerIcon({bg: 'red'}),
@@ -17,7 +17,7 @@ export default function LocalClientTrackLayer() {
             '': MarkerIcon({bg: 'yellow'}),
         }
     };
-    
+
     function addTrackToMap(track) {
         let layer = new L.GPX(track.gpx, {
             async: true,
@@ -25,12 +25,13 @@ export default function LocalClientTrackLayer() {
         }).on('loaded', function (e) {
             map.fitBounds(e.target.getBounds());
         }).addTo(map);
-        layers.push({name: track.name, layer: layer, active: true})
+
+        layers[track.name] = {layer: layer, active: true}
     }
 
     useEffect(() => {
         if (ctx.selectedGpxFile?.selected) {
-            let currLayer = layers.find(l => l.name === ctx.selectedGpxFile.name);
+            let currLayer = layers[ctx.selectedGpxFile.name];
             if (currLayer) {
                 map.fitBounds(currLayer.layer._info.bounds);
             }
@@ -38,9 +39,11 @@ export default function LocalClientTrackLayer() {
     }, [ctx.selectedGpxFile, ctx.setSelectedGpxFile]);
 
     useEffect(() => {
-        layers.forEach(l => l.active = false);
+        for (let l in layers) {
+            layers[l].active = false;
+        }
         Object.values(ctx.localClientsTracks).forEach((track) => {
-            let currLayer = layers.find(l => l.name === track.name);
+            let currLayer = layers[track.name]
             if (track.selected && !currLayer) {
                 addTrackToMap(track);
             } else if (currLayer) {
@@ -48,15 +51,14 @@ export default function LocalClientTrackLayer() {
             }
         });
 
-        let deletedLayers = new Set();
-        layers.forEach(l => {
-            if (!l.active) {
-                deletedLayers.add(l)
-                map.removeLayer(l.layer);
+        for (let l in layers) {
+            if (!layers[l].active) {
+                map.removeLayer(layers[l].layer);
+                delete layers[l];
             }
-        });
+        }
 
-        setLayers([...layers.filter(x => !deletedLayers.has(x))]);
+        setLayers({...layers});
 
     }, [ctx.localClientsTracks, ctx.setLocalClientsTracks]);
 }
