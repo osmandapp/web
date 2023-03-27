34996fc85d7e4ab5e71b4bcb699f8bef39cae16e
Eugene <prozmeu@gmail.com>
2023-03-27 12:33:36 +0200

    landing page

--- main/cycling/mtb/index.md
+++
@@ -1,4 +0,0 @@
----
-title: MTB
----
-We are always seeking for new contributors who can join our international team. Please check our [Mission](./mission.md) to see if you can help to make the World better.
--- main/src/pages/apps.js
+++ main/src/pages/apps.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import clsx from 'clsx';
 import Layout from '@theme/Layout';
+import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import styles from './index.module.css';
 
 import AllStores from '../components/buttons/AllStores.mdx';
+import Features from '../components/home/Features';
+import Poll from '../components/home/Poll';
+import Layout2Blocks from '../components/home/Layout2Blocks';
+import Twitter from '../components/home/Twitter';
+import Screenshots from '../components/home/Screenshots';
+import Impressions from '../components/home/Impressions';
+
 function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
   return (
@@ -25,8 +33,15 @@ export default function Home() {
   return (
     <Layout
       title={`${siteConfig.title}`}
-      description="Offline Mobile Maps &amp; Navigation">
+      description="Cycling">
       <HomepageHeader />
+      <main>
+
+        <Features />
+        
+
+        
+      </main>
     </Layout>
   );
 }
