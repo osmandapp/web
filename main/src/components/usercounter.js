43648b3e11589d8590f30373a4aec9a1d1e6484e
tanya12345 <tatoorel.adv@gmail.com>
2023-04-17 15:06:25 +0100

    supercounterfix

--- main/src/components/usercounter.js
+++ main/src/components/usercounter.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 
 function Counter() {
   const [count, setCount] = useState(0);
+  const [finishedCounting, setFinishedCounting] = useState(false);
 
   useEffect(() => {
     function handleScroll() {
@@ -9,12 +10,13 @@ function Counter() {
       const position = element.getBoundingClientRect().top;
       const windowHeight = window.innerHeight;
 
-      if (position < windowHeight) {
+      if (position < windowHeight && !finishedCounting) {
         // Долистали до элемента, начинаем счет
         let i = 0;
         const interval = setInterval(() => {
-          if (i > 10000000) {
+          if (i > 10500000) {
             clearInterval(interval);
+            setFinishedCounting(true);
           } else {
             setCount(i);
             i += 500000;
@@ -28,7 +30,7 @@ function Counter() {
     return () => {
       window.removeEventListener("scroll", handleScroll);
     };
-  }, []);
+  }, [finishedCounting]);
 
   return (
     <div style={{ fontSize: "2em", color: "#ff8800" }}>
@@ -37,14 +39,14 @@ function Counter() {
   );
 }
 
-export default function usercounter() {
+export default function Tanya() {
   return (
     <div>
       <p> </p>
       <div id="counter">
         <Counter />
       </div>
-      <p>Users tried OsmAnd</p>
+      <p>Ludzie próbowali</p>
     </div>
   );
 }
