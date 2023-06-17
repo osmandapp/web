a516e305268081468b61b64f93bae1a175a6436e
RZR-UA <youonly@ukr.net>
2023-06-17 23:08:23 +0200

    App.js add globalNavigate component

--- map/src/App.js
+++ map/src/App.js
@@ -3,14 +3,14 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 // components
 import OsmAndMapFrame from './drawer/components/OsmAndMapFrame';
 import LoginDialog from './login/LoginDialog';
+import { HttpApiLogout } from './login/HttpApiLogout';
 import { AppContextProvider } from './context/AppContext'
-
-
 
 const App = () => {
   return (
     <AppContextProvider>
       <BrowserRouter>
+        <HttpApiLogout />
         <Routes>
           <Route path="/map" element={<OsmAndMapFrame />}>
             <Route path="loginForm" element={<LoginDialog />}>
