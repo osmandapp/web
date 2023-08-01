e1473e063021bc0b2011add33e762fb1b86f5063
alisa911 <alisa.91150@gmail.com>
2023-08-01 12:32:29 +0300

    refactoring

--- map/src/context/FavoritesManager.js
+++ map/src/context/FavoritesManager.js
@@ -191,7 +191,10 @@ function createDefaultWptGroup(wptGroup) {
 function getGroupSize(group) {
     const wpts =
         group?.pointsGroups[group.name === DEFAULT_GROUP_NAME ? DEFAULT_GROUP_NAME_POINTS_GROUPS : group.name]?.points;
-    return wpts && wpts.length > 0 ? wpts.length : 0;
+    if (wpts) {
+        return wpts.length > 0 ? wpts.length : 0;
+    }
+    return 0;
 }
 
 const FavoritesManager = {
