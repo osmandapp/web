ea3fad68101167e48327fb65db127b85ba2a23e6
alisa911 <alisa.91150@gmail.com>
2023-08-17 16:35:09 +0300

    fix desc width mobile

--- map/src/infoblock/components/track/dialogs/DescTrackDialog.jsx
+++ map/src/infoblock/components/track/dialogs/DescTrackDialog.jsx
@@ -89,7 +89,7 @@ export default function DescTrackDialog({ dialogOpen, setDialogOpen }) {
         <>
             <Dialog disableEnforceFocus open={dialogOpen} onClose={toggleShowDialog}>
                 <DialogTitle>Description</DialogTitle>
-                <DialogContent sx={{ minWidth: 300, minHeight: editDescription ? 300 : 0 }}>
+                <DialogContent sx={{ minHeight: editDescription ? 300 : 0 }}>
                     {!editDescription ? (
                         <>
                             {link ? (
@@ -118,7 +118,7 @@ export default function DescTrackDialog({ dialogOpen, setDialogOpen }) {
                                     Click EDIT to add a description...
                                 </Alert>
                             )}
-                            <DialogContentText component={'span'}>
+                            <DialogContentText sx={{ width: '100%' }}>
                                 <div dangerouslySetInnerHTML={{ __html: description }} />
                             </DialogContentText>
                         </>
