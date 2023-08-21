5634c85c2f92ad859703663aca718d96c5699b1e
alisa911 <alisa.91150@gmail.com>
2023-08-21 12:04:06 +0300

    fix style

--- map/src/drawer/components/OsmAndDrawer.js
+++ map/src/drawer/components/OsmAndDrawer.js
@@ -15,7 +15,6 @@ import { makeStyles } from '@material-ui/core/styles';
 const useStyles = makeStyles({
     btn: {
         width: '100%',
-        maxWidth: '350px',
         justifyContent: 'left !important',
         wordWrap: 'break-word',
         display: 'block !important',
