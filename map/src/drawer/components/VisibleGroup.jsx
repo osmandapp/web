dcb81d29b7e7acb8b413d1e9554f73421b7533f7
RZR-UA <youonly@ukr.net>
2023-08-28 21:20:34 +0200

    Add A-Z sorting for Visible tracks (local - cloud)

--- map/src/drawer/components/VisibleGroup.jsx
+++ map/src/drawer/components/VisibleGroup.jsx
@@ -150,20 +150,26 @@ export default function VisibleGroup({ visibleTracks, setVisibleTracks }) {
             <Collapse in={visibleTracksOpen} timeout="auto" unmountOnExit>
                 <div style={{ maxHeight: '41vh', overflow: 'auto' }}>
                     {visibleTracks.local.length > 0 &&
-                        visibleTracks.local.map((track, index) => {
-                            return <LocalTrackItem className={classes.item} key={'vis-local-' + index} track={track} />;
-                        })}
+                        visibleTracks.local
+                            .sort((a, b) => a.name > b.name)
+                            .map((track, index) => {
+                                return (
+                                    <LocalTrackItem className={classes.item} key={'vis-local-' + index} track={track} />
+                                );
+                            })}
                     {visibleTracks.cloud.length > 0 &&
-                        visibleTracks.cloud.map((track, index) => {
-                            return (
-                                <CloudTrackItem
-                                    key={'vis-cloud-' + index}
-                                    className={classes.item}
-                                    customIcon={<CloudOutlinedIcon fontSize="small" sx={{ mb: '-3px', mr: 1 }} />}
-                                    file={track}
-                                />
-                            );
-                        })}
+                        visibleTracks.cloud
+                            .sort((a, b) => a.name > b.name)
+                            .map((track, index) => {
+                                return (
+                                    <CloudTrackItem
+                                        key={'vis-cloud-' + index}
+                                        className={classes.item}
+                                        customIcon={<CloudOutlinedIcon fontSize="small" sx={{ mb: '-3px', mr: 1 }} />}
+                                        file={track}
+                                    />
+                                );
+                            })}
                 </div>
             </Collapse>
         </div>
