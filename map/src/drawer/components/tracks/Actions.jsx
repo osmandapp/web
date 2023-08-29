538ce6a3d8a77c89242b7a761e06f6f2579986fe
RZR-UA <youonly@ukr.net>
2023-08-29 18:37:59 +0200

    Add sort track byTime, use AZ when items are equal

--- map/src/drawer/components/tracks/Actions.jsx
+++ map/src/drawer/components/tracks/Actions.jsx
@@ -1,7 +1,9 @@
 import { Box, IconButton, Tooltip } from '@mui/material';
-// import { getGpxTime } from '../../../context/AppContext';
+import { getGpxTime } from '../../../context/TracksManager';
 import { SortByAlpha, Update, History, CallMade, CallReceived } from '@mui/icons-material';
 import { useState, useEffect } from 'react';
+
+const az = (a, b) => (a > b) - (a < b);
 
 function byAlpha(files, reverse) {
     const sortedCopy = [...files].sort((a, b) => {
@@ -13,14 +15,24 @@ function byAlpha(files, reverse) {
 }
 
 function byTime(files, reverse) {
-    console.log('time', reverse);
-    return files;
+    const sortedCopy = [...files].sort((a, b) => {
+        const A = getGpxTime(a);
+        const B = getGpxTime(b);
+        if (A === B) {
+            return az(a.name, b.name);
+        }
+        return reverse ? B - A : A - B;
+    });
+    return sortedCopy;
 }
 
 function byDistance(files, reverse) {
     const sortedCopy = [...files].sort((a, b) => {
         const A = a.analysis?.totalDistance ?? a.details?.analysis?.totalDistance ?? 0;
         const B = b.analysis?.totalDistance ?? b.details?.analysis?.totalDistance ?? 0;
+        if (A === B) {
+            return az(a.name, b.name);
+        }
         return reverse ? B - A : A - B;
     });
     return sortedCopy;
@@ -92,47 +104,4 @@ export default function Actions({ files, setSortFiles }) {
     }
 
     return <Box sx={{ ml: 4 }}>{icons}</Box>;
-
-    // return (
-    //     <div>
-    //         <IconButton
-    //             sx={{ ml: 4 }}
-    //             onClick={(e) => {
-    //                 e.stopPropagation();
-    //                 const sortedCopy = [...files].sort((f, s) => {
-    //                     let ftime = getGpxTime(f);
-    //                     let stime = getGpxTime(s);
-    //                     if (ftime === stime) {
-    //                         return f.name > s.name ? 1 : -1;
-    //                     }
-    //                     if (timeUp) {
-    //                         return ftime > stime ? 1 : -1;
-    //                     } else {
-    //                         return ftime < stime ? 1 : -1;
-    //                     }
-    //                 });
-    //                 setTimeUp(!timeUp);
-    //                 setSortFiles(sortedCopy);
-    //             }}
-    //         >
-    //             <Sort fontSize="small" />
-    //         </IconButton>
-    //         <IconButton
-    //             onClick={(e) => {
-    //                 e.stopPropagation();
-    //                 const sortedCopy = [...files].sort((f, s) => {
-    //                     if (alphaUp) {
-    //                         return f.name > s.name ? 1 : -1;
-    //                     } else {
-    //                         return f.name < s.name ? 1 : -1;
-    //                     }
-    //                 });
-    //                 setAlphaUp(!alphaUp);
-    //                 setSortFiles(sortedCopy);
-    //             }}
-    //         >
-    //             <SortByAlpha fontSize="small" />
-    //         </IconButton>
-    //     </div>
-    // );
 }
