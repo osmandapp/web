08bc5e32433246fa1745010df5373345a0ffa019
RZR-UA <youonly@ukr.net>
2023-09-13 15:59:12 +0200

    Add action actionLogIn(), add test 01-test-login

--- tests/selenium/src/tests/01-test-login.mjs
+++ tests/selenium/src/tests/01-test-login.mjs
@@ -1,9 +1,9 @@
 'use strict';
 
 import actionOpenMap from '../actions/actionOpenMap.mjs';
-// import actionLogIn from '../actions/actionLogIn.mjs';
+import actionLogIn from '../actions/actionLogIn.mjs';
 
 export default async function test() {
     await actionOpenMap();
-    // await actionLogIn();
+    await actionLogIn();
 }
