023503706062b9389bfbc8b73ea05839a9a3d6df
RZR-UA <youonly@ukr.net>
2023-09-13 12:32:33 +0200

    Test Cloud acc created

--- tests/selenium/src/settings.mjs
+++ tests/selenium/src/settings.mjs
@@ -4,5 +4,5 @@ export const IMPICIT_WAIT = 1000; // driver.implicitlyWait - used for quick-look
 
 export const TIMEOUT_WAIT = 10000; // driver.wait() timeout - used to wait for loading-conditions (ms)
 
-// export const TEST_LOGIN = 'osmand@mailinator.com';
-// export const TEST_PASSWORD = '0xDEADBEEF';
+export const TEST_LOGIN = 'osmand@sharklasers.com';
+export const TEST_PASSWORD = '0xDEADBEEF';
