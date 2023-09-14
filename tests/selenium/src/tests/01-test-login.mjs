021ac3cc2392b35cbea3692a749ac39711559e30
RZR-UA <youonly@ukr.net>
2023-09-14 20:40:20 +0200

    Add actionFinish() to 01-test-login test

--- tests/selenium/src/tests/01-test-login.mjs
+++ tests/selenium/src/tests/01-test-login.mjs
@@ -2,8 +2,10 @@
 
 import actionOpenMap from '../actions/actionOpenMap.mjs';
 import actionLogIn from '../actions/actionLogIn.mjs';
+import actionFinish from '../actions/actionFinish.mjs';
 
 export default async function test() {
     await actionOpenMap();
     await actionLogIn();
+    await actionFinish();
 }
