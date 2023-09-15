a55a47418f0a2545d71db939074cc55418e5ab0b
RZR-UA <youonly@ukr.net>
2023-09-15 15:51:48 +0200

    Add test 40-route-by-gpx and SE-ids for RouteMenu

---
+++ tests/selenium/src/tests/40-route-by-gpx.mjs
@@ -0,0 +1,56 @@
+'use strict';
+
+import { resolve } from 'node:path';
+import { By } from 'selenium-webdriver';
+
+import { enclose, waitBy, clickBy, navigateHash } from '../lib.mjs';
+
+import actionOpenMap from '../actions/actionOpenMap.mjs';
+
+const TEST_GPX_FILE = 'test-routed-osmand.gpx';
+const GO_CENTER_HASH = '#10/48.4716/23.7064';
+const CHECK_START = '48.61650, 23.68181';
+const CHECK_END = '48.61421, 23.70352';
+const CAR = '157.7 km';
+const BIKE = '162.3 km';
+const FOOT = '161.1 km';
+
+export default async function test() {
+    await actionOpenMap();
+
+    await clickBy(By.id('se-show-main-menu'), { optional: true });
+    await clickBy(By.id('se-show-menu-route'), { optional: true });
+
+    // upload and move map to the center of gpx track
+    await enclose(uploader, { tag: 'upload gpx' });
+    await navigateHash(GO_CENTER_HASH);
+
+    // validate start, end points, and profiles
+    await enclose(checkStartEndPoints, { tag: 'check start-end points' });
+    await validateRouteInfo('pedestrian', FOOT);
+    await validateRouteInfo('bicycle', BIKE);
+    await validateRouteInfo('car', CAR);
+}
+
+const validateRouteInfo = async (profile, grep) => {
+    const validate = async () => {
+        const text = await (await waitBy(By.id('se-route-info'))).getText();
+        return text.toString().includes(grep);
+    };
+
+    await clickBy(By.id('se-route-select'));
+    await clickBy(By.id('se-route-profile-' + profile));
+    await enclose(validate, { tag: `check ${profile} for ${grep}` });
+};
+
+const checkStartEndPoints = async () => {
+    const start = await (await waitBy(By.id('se-route-start-point'))).getAttribute('value');
+    const end = await (await waitBy(By.id('se-route-end-point'))).getAttribute('value');
+    return start === CHECK_START && end === CHECK_END;
+};
+
+const uploader = async () => {
+    const element = await waitBy(By.id('contained-button-route'));
+    await element.sendKeys(resolve('gpx', TEST_GPX_FILE));
+    return true;
+};
