a547f1f86a3b78d6002986ec82a566a0993dc57e
RZR-UA <youonly@ukr.net>
2023-10-04 16:25:26 +0200

    Add console tag to measure() to use with () => ()

--- map/src/util/Utils.js
+++ map/src/util/Utils.js
@@ -241,7 +241,7 @@ export function prepareFileName(filename) {
  *
  * measure(getPoints).forEach(...) // measure performance
  */
-export function measure(f, ms = 1000) {
+export function measure(f, tag, ms = 1000) {
     let counter = 0;
     let result = null;
     const started = Date.now();
@@ -250,7 +250,12 @@ export function measure(f, ms = 1000) {
         result = f();
     } while (Date.now() < started + ms);
     const delta = Date.now() - started;
-    console.debug(f.name, '~', parseFloat(Number(counter * (ms / delta) * (1000 / ms)).toFixed(2)), 'per second');
+    console.debug(
+        tag ?? f.name,
+        '~',
+        parseFloat(Number(counter * (ms / delta) * (1000 / ms)).toFixed(2)),
+        'per second'
+    );
     return result;
 }
 
