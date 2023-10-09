e4a626829c49b9194fc71cbccddedb73f3c95b22
alisa911 <alisa.91150@gmail.com>
2023-10-09 11:40:30 +0300

    fix styles

--- map/src/menu/MainMenu.js
+++ map/src/menu/MainMenu.js
@@ -179,7 +179,7 @@ export default function MainMenu({
                             width: size,
                             overflow: 'hidden',
                             zIndex: 1250,
-                            borderRight: !menuInfo && 'none !important',
+                            borderRight: (!menuInfo || (menuInfo && openMainMenu)) && 'none !important',
                             boxShadow:
                                 !menuInfo || (menuInfo && openMainMenu)
                                     ? '0px 8px 10px -5px rgba(0,0,0,0.2), 0px 16px 24px 2px rgba(0,0,0,0.14), 0px 6px 10px 5px rgba(0,0,0,0.12);'
