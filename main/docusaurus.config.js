6f233f30216a461a60a769a709c1e2a5e1a052bb
RZR-UA <youonly@ukr.net>
2023-10-09 09:16:06 +0200

    Fix main menu /map absolute url to relative

--- main/docusaurus.config.js
+++ main/docusaurus.config.js
@@ -58,9 +58,8 @@ const config = {
           },
           { to: '/blog', label: 'Blog', position: 'left'},
           { to: '/docs/user/purchases', label: '💳 Purchases', position: 'left' },
-          { to: 'https://osmand.net/map', label: '🌍 Map', position: 'left' },
+          { to: '/map', label: '🌍 Map', position: 'left' },
           { to: '/docs/hiring', label: '🚵‍♂️ Join us', position: 'left' },
-//          { href: 'https://osmand.net/map', label: 'Map', position: 'left' },
           {
             href: 'https://github.com/osmandapp/',
             label: 'GitHub',
@@ -80,7 +79,7 @@ const config = {
               },
               {
                 label: 'Map 🌍',
-                to: 'https://osmand.net/map',
+                to: '/map',
               },
               {
                 label: 'Docs',
@@ -98,7 +97,7 @@ const config = {
                 label: 'Build it',
                 to: '/docs/build-it',
               },
-              
+
               {
                 label: 'Giveaway',
                 to: '/giveaway',
@@ -135,7 +134,7 @@ const config = {
               {
                 label: 'Matrix',
                 href: 'https://matrix.to/#/#osmand:hacklab.fi',
-              }, 
+              },
             ],
           },
           {
@@ -180,7 +179,7 @@ const config = {
               {
                 label: 'Türkçe',
                 href: 'https://t.me/OsmAndTR',
-              },                
+              },
             ],
           },
           {
