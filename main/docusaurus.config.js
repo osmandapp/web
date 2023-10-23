dc795359a7572105cb9e20d91de444c2c0f13f65
Eugene <prozmeu@gmail.com>
2023-10-23 22:08:47 +0200

    update link to Redbubble

--- main/docusaurus.config.js
+++ main/docusaurus.config.js
@@ -200,7 +200,7 @@ const config = {
               },
               {
                 label: 'Get OsmAnd Merchandise',
-                href: 'https://www.redbubble.com/people/osmand/works/26426633-osmand',
+                href: 'https://www.redbubble.com/people/osmand/explore?asc=u&page=1&sortOrder=recent',
               },
               {
                 label: 'support@osmand.net',
