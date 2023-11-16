16ad10d8d8e025a9054539fd054f5d5161a5fb2b
alisa911 <alisa.91150@gmail.com>
2023-11-16 12:52:20 +0200

    add empty login component

--- map/src/menu/errors/Empty.jsx
+++ map/src/menu/errors/Empty.jsx
@@ -4,30 +4,37 @@ import styles from './errors.module.css';
 import CloudGpxUploader from '../../frame/components/util/CloudGpxUploader';
 import { useContext } from 'react';
 import AppContext from '../../context/AppContext';
+import EmptyLogin from './EmptyLogin';
 
 export default function Empty({ title, text, folder = null }) {
     const ctx = useContext(AppContext);
 
     return (
-        <Box className={styles.block}>
-            <Icon className={styles.icon}>
-                <EmptyIcon className={styles.icon} />
-            </Icon>
-            <Box className={styles.info}>
-                <ListItemText disableTypography={true} className={styles.title}>
-                    {title}
-                </ListItemText>
-                <ListItemText disableTypography={true} className={styles.text}>
-                    {text}
-                </ListItemText>
-            </Box>
-            {ctx.loginUser && folder && (
-                <CloudGpxUploader folder={folder} style={styles.label}>
-                    <Button className={styles.button} component="span">
-                        Import
-                    </Button>
-                </CloudGpxUploader>
+        <>
+            {ctx.loginUser ? (
+                <Box className={styles.block}>
+                    <Icon className={styles.icon}>
+                        <EmptyIcon className={styles.icon} />
+                    </Icon>
+                    <Box className={styles.info}>
+                        <ListItemText disableTypography={true} className={styles.title}>
+                            {title}
+                        </ListItemText>
+                        <ListItemText disableTypography={true} className={styles.text}>
+                            {text}
+                        </ListItemText>
+                    </Box>
+                    {folder && (
+                        <CloudGpxUploader folder={folder} style={styles.label}>
+                            <Button className={styles.button} component="span">
+                                Import
+                            </Button>
+                        </CloudGpxUploader>
+                    )}
+                </Box>
+            ) : (
+                <EmptyLogin />
             )}
-        </Box>
+        </>
     );
 }
---
+++ map/src/menu/errors/EmptyLogin.jsx
@@ -0,0 +1,34 @@
+import { useContext } from 'react';
+import AppContext from '../../context/AppContext';
+import { Box, Button, Icon, ListItemText } from '@mui/material';
+import styles from './errors.module.css';
+import CloudGpxUploader from '../../frame/components/util/CloudGpxUploader';
+import { ReactComponent as UserAccountIcon } from '../../assets/icons/ic_action_user_account.svg';
+import { useNavigate } from 'react-router-dom';
+export default function EmptyLogin() {
+    const ctx = useContext(AppContext);
+
+    const navigate = useNavigate();
+    const openLogin = () => {
+        navigate('/map/loginForm' + window.location.search + window.location.hash);
+    };
+
+    return (
+        <Box className={styles.block}>
+            <Icon className={styles.icon}>
+                <UserAccountIcon className={styles.icon} />
+            </Icon>
+            <Box className={styles.info}>
+                <ListItemText disableTypography={true} className={styles.title}>
+                    OsmAnd Account
+                </ListItemText>
+                <ListItemText disableTypography={true} className={styles.text}>
+                    Manage your data across devices with an OsmAnd account.
+                </ListItemText>
+            </Box>
+            <Button className={styles.button} component="span" onClick={openLogin}>
+                Login
+            </Button>
+        </Box>
+    );
+}
