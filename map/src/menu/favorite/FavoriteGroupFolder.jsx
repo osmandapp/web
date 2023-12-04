246cd2451a9bcaa807511bffe6afc9e09968509b
alisa911 <alisa.91150@gmail.com>
2023-12-04 21:10:16 +0200

    fix if currentLoc null

--- map/src/menu/favorite/FavoriteGroupFolder.jsx
+++ map/src/menu/favorite/FavoriteGroupFolder.jsx
@@ -10,7 +10,7 @@ import Utils, { getDistance } from '../../util/Utils';
 import TracksManager from '../../manager/track/TracksManager';
 import Loading from '../errors/Loading';
 import { useLocation } from '../../util/hooks/useLocation';
-import {isEmpty} from "lodash";
+import { isEmpty } from 'lodash';
 
 export default function FavoriteGroupFolder({ folder }) {
     const ctx = useContext(AppContext);
