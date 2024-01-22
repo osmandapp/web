07f9254ce74b1fcdb1151b31aa80ea37e1b65d8c
RZR-UA <youonly@ukr.net>
2024-01-22 12:47:34 +0100

    Refactor docmap into help-structure-generated.json

--- main/scripts/help-structure-generator.js
+++ main/scripts/help-structure-generator.js
@@ -1,33 +1,47 @@
 const fs = require("fs-extra");
 
-module.exports = OsmAndDocMapGenerator;
+module.exports = OsmAndHelpStructureGenerator;
 
-const BASE_URL = "/docs/"; // relative or absolute URL to include into JSON docmap
+const BASE_URL = "/docs/"; // relative or absolute URL to include into JSON articles
 
 const DOC_PATH = "./docs/"; // path to docs files when yarn build started
-const JSON_DOCMAP = "./build/docmap.json"; // output JSON file with the Documentation Map
+const JSON_OUTPUT_FILE = "./build/help-structure-generated.json"; // JSON file with the Help Structure
 const BUILD_DIR_READY_TRIGGER = "./build/assets"; // trigger file/dir that indicates that build dir is ready
 
-async function OsmAndDocMapGenerator(props) {
+// repository-maintained files with help links
+const IOS_LINKS_JSON = "./static/help-links-ios.json";
+const ANDROID_LINKS_JSON = "./static/help-links-android.json";
+
+async function OsmAndHelpStructureGenerator(props) {
   const items = await props.defaultSidebarItemsGenerator(props);
 
-  const docmap = [];
-  traverseItems(docmap, items);
-  flushDocMapIntoBuildDir(docmap);
-  // docmap.forEach((i) => console.log(i)); // debug
+  const articles = [];
+  traverseItems(articles, items);
+  flushArticlesIntoBuildDir(articles);
+  // articles.forEach((i) => console.log(i)); // debug
 
   return items;
 }
 
 // Docusaurus cleanups the ./build dir instantly after this module called.
-// That is the reason why flushing JSON docmap file immediately is a wrong way.
+// That is the reason why flushing JSON file into ./build/ immediately is a wrong way.
 // Docusaurus does not have API to connect custom sidebar generator with the later-on build process.
 // As a result, cycle of setTimeout() is used to wait for "trigger" file in the build dir to assure that build is started.
-function flushDocMapIntoBuildDir(docmap) {
+function flushArticlesIntoBuildDir(articles) {
   const prettyJSON = (smth) => JSON.stringify(smth, null, 2);
+
+  const ios = JSON.parse(fs.readFileSync(IOS_LINKS_JSON, { encoding: 'utf8' }));
+  const android = JSON.parse(fs.readFileSync(ANDROID_LINKS_JSON, { encoding: 'utf8' }));
+
+  const helpStructure = {
+    articles,
+    android,
+    ios,
+  }
+
   function flush() {
     if (fs.existsSync(BUILD_DIR_READY_TRIGGER)) {
-      fs.writeFileSync(JSON_DOCMAP, prettyJSON(docmap), { encoding: "utf8" });
+      fs.writeFileSync(JSON_OUTPUT_FILE, prettyJSON(helpStructure), { encoding: "utf8" });
       return;
     }
     setTimeout(flush, 1000);
@@ -36,15 +50,15 @@ function flushDocMapIntoBuildDir(docmap) {
 }
 
 // traverse items array
-function traverseItems(docmap, items, level = 1) {
+function traverseItems(articles, items, level = 1) {
   items.forEach((i) => {
-    i && dumpItem(docmap, i, level); // dump current item
-    i && i.items && traverseItems(docmap, i.items, level + 1); // recurse
+    i && dumpItem(articles, i, level); // dump current item
+    i && i.items && traverseItems(articles, i.items, level + 1); // recurse
   });
 }
 
 // dump one category/article
-function dumpItem(docmap, item, level) {
+function dumpItem(articles, item, level) {
   const { id } = item.link ?? item; // categories get { id } from `item.link` but articles get from `item` directly
 
   if (id === undefined) {
@@ -56,7 +70,7 @@ function dumpItem(docmap, item, level) {
   const label = item.label ?? readTitle(file, id); // label might be not available, so read it from the file
   const url = (BASE_URL + id).replace(/\/index$/, ""); // remove extra /index from the tail (uri/index -> uri)
 
-  docmap.push({
+  articles.push({
     level,
     type,
     label,
