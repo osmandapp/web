bd50473847457553516d125d62b4dcbe23310236
RZR-UA <youonly@ukr.net>
2024-01-22 10:15:19 +0100

    Fix calculateGpxRoute when empty route received

--- map/src/store/geoRouter/legacy/calculateGpxRoute.js
+++ map/src/store/geoRouter/legacy/calculateGpxRoute.js
@@ -32,21 +32,24 @@ export async function calculateGpxRoute({ routeTrackFile, changeRouteText, setRo
     if (response.ok) {
         let data = await response.json();
         let start, finish;
-        if (data?.features?.length > 0) {
-            let coords = data?.features[0].geometry.coordinates;
-            if (coords.length > 0) {
-                start = { lat: coords[0][1], lng: coords[0][0] };
-                finish = { lat: coords[coords.length - 1][1], lng: coords[coords.length - 1][0] };
+        if (data?.features?.length > 0 && data.features[0].geometry?.coordinates?.length > 0) {
+            const coords = data.features[0].geometry.coordinates;
+            start = { lat: coords[0][1], lng: coords[0][0] };
+            finish = { lat: coords[coords.length - 1][1], lng: coords[coords.length - 1][0] };
+            if (start && finish) {
+                this.setOption('route.points.start', start);
+                this.setOption('route.points.finish', finish);
+                this.setOption('route.points.viaPoints', []);
+                const { route } = this.putRoute({ route: data });
+                changeRouteText(false, this.getRouteProps(route));
+            } else {
+                changeRouteText(false, null);
+                setRoutingErrorMsg('gpx-approximate no start/finish');
             }
-            const { route } = this.putRoute({ route: data });
-            this.setOption('route.points.start', start);
-            this.setOption('route.points.finish', finish);
-            this.setOption('route.points.viaPoints', []);
-            changeRouteText(false, this.getRouteProps(route));
         } else {
             this.resetRoute();
             changeRouteText(false, null);
-            setRoutingErrorMsg('gpx-approximate error');
+            setRoutingErrorMsg('gpx-approximate empty result');
         }
     } else {
         const message = await response.text();
