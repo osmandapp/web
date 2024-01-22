9e134c2635328ee707bbc8c26eea72eb6497c8ce
alisa911 <alisa.91150@gmail.com>
2024-01-22 10:03:08 +0200

    Fix tests 33 34

--- tests/selenium/src/tests/33-visible-tracks.mjs
+++ tests/selenium/src/tests/33-visible-tracks.mjs
@@ -1,7 +1,7 @@
 import actionOpenMap from '../actions/actionOpenMap.mjs';
 import actionLogIn from '../actions/actionLogIn.mjs';
 import actionAddOneTrack from '../actions/actionAddOneTrack.mjs';
-import { clickBy, waitBy, waitByRemoved } from '../lib.mjs';
+import { clickBy, waitBy, waitByRemoved, checkElementByCss } from '../lib.mjs';
 import { By } from 'selenium-webdriver';
 import actionFinish from '../actions/actionFinish.mjs';
 import { deleteTrack } from '../util.mjs';
@@ -19,15 +19,15 @@ export default async function test() {
     await waitBy(By.id('se-track-actions'));
     await clickBy(By.id('se-show-on-map-action'));
 
-    await waitByRemoved(By.id(' se-infoblock-all'));
-    await waitBy(By.className(' leaflet-interactive'));
+    await waitByRemoved(By.id('se-infoblock-all'));
+    await checkElementByCss('img[src*="point_finish"]');
 
     await clickBy(By.id(`se-actions-${trackName}`));
     await waitBy(By.id('se-track-actions'));
     await clickBy(By.id('se-hide-from-map-action'));
 
-    await waitByRemoved(By.id(' se-infoblock-all'));
-    await waitByRemoved(By.className(' leaflet-interactive'));
+    await waitByRemoved(By.id('se-infoblock-all'));
+    await checkElementByCss('img[src*="point_finish"]', false);
 
     // check visible tracks
     await clickBy(By.id(`se-actions-${trackName}`));
@@ -37,7 +37,7 @@ export default async function test() {
     await clickBy(By.id('se-visible-tracks-menu'));
     await waitBy(By.id(`se-new-visible-track-${trackName}`));
     await clickBy(By.id(`se-visible-switch-${trackName}`));
-    await waitByRemoved(By.className('leaflet-interactive'));
+    await checkElementByCss('img[src*="point_finish"]', false);
 
     // check recently visible tracks
     await clickBy(By.id('se-close-visible-tracks'));
@@ -46,12 +46,12 @@ export default async function test() {
     await waitBy(By.id(`se-old-visible-track-${trackName}`));
     await waitBy(By.id('se-empty-visible'));
     await clickBy(By.id(`se-visible-switch-${trackName}`));
-    await waitBy(By.className(' leaflet-interactive'));
+    await waitBy(By.className('leaflet-interactive'));
     await waitBy(By.id(`se-new-visible-track-${trackName}`));
 
     // check hide all
     await clickBy(By.id('se-hide-all-visible-tracks'));
-    await waitByRemoved(By.className(' leaflet-interactive'));
+    await checkElementByCss('img[src*="point_finish"]', false);
 
     await clickBy(By.id('se-close-visible-tracks'));
     await deleteTrack(`${trackName}`);
