42a0bff9438d064a0dfa0df4e255a229a947e765
alisa911 <alisa.91150@gmail.com>
2024-02-22 18:01:09 +0200

    add forecast table styles

--- map/src/menu/weather/ForecastTable.jsx
+++ map/src/menu/weather/ForecastTable.jsx
@@ -1,10 +1,13 @@
 import React, { useContext, useEffect } from 'react';
 import AppContext from '../../context/AppContext';
-import { ListItemIcon, ListItemText, MenuItem, Typography } from '@mui/material';
+import { Divider, ListItemIcon, ListItemText, MenuItem, Typography } from '@mui/material';
 import { dayFormatter, timeFormatter } from '../../manager/WeatherManager';
+import styles from '../weather/weather.module.css';
 
 export default function ForecastTable({ dayForecast, weekForecast, currentTimeForecast, setHeaderForecast }) {
     const ctx = useContext(AppContext);
+
+    const NOT_AVAILABLE = 'Not available';
 
     function getForecastValue(item) {
         function formatting(value) {
@@ -20,6 +23,8 @@ export default function ForecastTable({ dayForecast, weekForecast, currentTimeFo
             if (res?.length > 0) {
                 if (item.index !== -1) {
                     return `${formatting(res[0][item.index])} ${item.units}`;
+                } else {
+                    return NOT_AVAILABLE;
                 }
             }
         }
@@ -27,6 +32,7 @@ export default function ForecastTable({ dayForecast, weekForecast, currentTimeFo
 
     const ForecastItem = ({ item, index }) => {
         const forecastValue = getForecastValue(item);
+        const isLastItem = index === ctx.weatherLayers[ctx.weatherType].length - 1;
 
         useEffect(() => {
             if (forecastValue) {
@@ -37,34 +43,36 @@ export default function ForecastTable({ dayForecast, weekForecast, currentTimeFo
         }, [forecastValue]);
 
         return (
-            <MenuItem key={index}>
-                <ListItemIcon>{item.icon}</ListItemIcon>
-                <ListItemText>
-                    <div
-                        style={{
-                            display: 'flex',
-                            alignItems: 'center',
-                            justifyContent: 'space-between',
-                        }}
-                    >
-                        <Typography variant="inherit" noWrap>
+            <>
+                <MenuItem disabled={forecastValue === NOT_AVAILABLE} className={styles.forecastItem} key={index}>
+                    <ListItemIcon className={styles.forecastIcon}>{item.icon}</ListItemIcon>
+                    <ListItemText>
+                        <Typography sx={{ color: '#237BFF !important' }} variant="inherit">
                             {item.name()}
                         </Typography>
-                        <Typography variant="body2" noWrap>
-                            {forecastValue}
+                    </ListItemText>
+                    <div style={{ display: 'flex' }}>
+                        <Typography variant="body2" className={styles.forecastData}>
+                            {forecastValue === NOT_AVAILABLE ? '' : forecastValue?.split(' ')[0]}
+                        </Typography>
+                        <Typography variant="body2" className={styles.forecastUnit}>
+                            {forecastValue === NOT_AVAILABLE ? forecastValue : forecastValue?.split(' ')[1]}
                         </Typography>
                     </div>
-                </ListItemText>
-            </MenuItem>
+                </MenuItem>
+                {!isLastItem && <Divider className={styles.forecastTableDivider} />}
+            </>
         );
     };
 
     return (
         <>
+            <Divider />
             {ctx.weatherLayers &&
                 ctx.weatherLayers[ctx.weatherType].map((item, index) => (
                     <ForecastItem item={item} index={index} key={index} />
                 ))}
+            <Divider sx={{ mt: '0px !important' }} />
         </>
     );
 }
