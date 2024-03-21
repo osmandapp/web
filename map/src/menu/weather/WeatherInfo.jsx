28e059378b650c7f6747544cae290f9e17dedc20
alisa911 <alisa.91150@gmail.com>
2024-03-21 15:31:16 +0200

    fix weather info

--- map/src/menu/weather/WeatherInfo.jsx
+++ map/src/menu/weather/WeatherInfo.jsx
@@ -4,41 +4,52 @@ import { currentDiffHours } from '../../manager/WeatherManager';
 import AppContext from '../../context/AppContext';
 import styles from '../weather/weather.module.css';
 
-export default function WeatherInfo() {
+export default function WeatherInfo({ dayForecast, weekForecast }) {
     const ctx = useContext(AppContext);
 
     const [resultText, setResultText] = useState(null);
 
     useEffect(() => {
-        let weatherDateObj = ctx.weatherDate;
-        let hourstr = 'now';
-        let hours = currentDiffHours(ctx, weatherDateObj);
+        const useDayForecast = ctx.weatherDate.getDay() === new Date().getDay();
+        const forecast = useDayForecast ? dayForecast : weekForecast;
+        if (ctx.weatherDate && forecast) {
+            if (
+                Array.isArray(forecast) &&
+                forecast.length > 0 &&
+                Array.isArray(forecast[0]) &&
+                forecast[0].length > 7
+            ) {
+                const time = forecast[0][7];
+                const weatherDateObj = new Date(time);
+                let hourstr = 'now';
+                let hours = currentDiffHours(ctx, weatherDateObj);
 
-        if (hours !== 0) {
-            let day = 0;
-            while (hours >= 24) {
-                day++;
-                hours -= 24;
-            }
-            if (day > 0) {
-                if (day === 1) {
-                    hourstr = '+ ' + day + ' day ';
-                } else {
-                    hourstr = '+ ' + day + ' days ';
+                if (hours !== 0) {
+                    let day = 0;
+                    while (hours >= 24) {
+                        day++;
+                        hours -= 24;
+                    }
+                    if (day > 0) {
+                        if (day === 1) {
+                            hourstr = '+ ' + day + ' day ';
+                        } else {
+                            hourstr = '+ ' + day + ' days ';
+                        }
+                        hours = hours - (hours % 3);
+                    } else if (hours > 0) {
+                        hourstr = '+';
+                    }
+                    if (hours > 0) {
+                        hourstr += hours + ' hours';
+                    } else if (hours < 0) {
+                        hourstr = hours + ' hours';
+                    }
                 }
-                hours = hours - (hours % 3);
-            } else if (hours > 0) {
-                hourstr = '+';
-            }
-            if (hours > 0) {
-                hourstr += hours + ' hours';
-            } else if (hours < 0) {
-                hourstr = hours + ' hours';
+                setResultText(`${weatherDateObj.toDateString()}  ${weatherDateObj.getHours()}:00 [${hourstr}].`);
             }
         }
-
-        setResultText(`${weatherDateObj.toDateString()}  ${weatherDateObj.getHours()}:00 [${hourstr}].`);
-    }, [ctx.weatherDate, ctx.setWeatherDate, ctx.setWeatherLayers]);
+    }, [dayForecast, weekForecast]);
 
     return (
         <ListItem id="se-weather-info" className={styles.weatherInfoBlock}>
