797ab37c3a9d5344605a714cad7ec88465d27e4f
alisa911 <alisa.91150@gmail.com>
2024-04-16 10:14:24 +0300

    add tooltips

--- map/src/infoblock/components/wpt/WptDetailsButtons.jsx
+++ map/src/infoblock/components/wpt/WptDetailsButtons.jsx
@@ -1,5 +1,5 @@
 import styles from '../../infoblock.module.css';
-import { Box, IconButton } from '@mui/material';
+import { Box, IconButton, Tooltip } from '@mui/material';
 import TracksManager from '../../../manager/track/TracksManager';
 import React, { useContext, useState } from 'react';
 import EditWptDialog from '../../../dialogs/favorites/EditWptDialog';
@@ -7,9 +7,11 @@ import DeleteWptDialog from '../../../dialogs/favorites/DeleteWptDialog';
 import { ReactComponent as DeleteIcon } from '../../../assets/icons/ic_action_delete_outlined.svg';
 import { ReactComponent as EditIcon } from '../../../assets/icons/ic_action_edit_outlined.svg';
 import AppContext from '../../../context/AppContext';
+import { useTranslation } from 'react-i18next';
 
 export default function WptDetailsButtons({ wpt, isDetails }) {
     const ctx = useContext(AppContext);
+    const { t } = useTranslation();
 
     const [editWptDialogOpen, setEditWptDialogOpen] = useState(false);
     const [deleteWptDialogOpen, setDeleteWptDialogOpen] = useState(false);
@@ -18,44 +20,48 @@ export default function WptDetailsButtons({ wpt, isDetails }) {
         <>
             <Box className={styles.wptActionButtonContainer}>
                 {!wpt.type?.isPoi && (
-                    <IconButton
-                        className={styles.wptActionsButtons}
-                        onClick={() => {
-                            if (wpt.type.isWpt) {
-                                TracksManager.handleEditCloudTrack(ctx).then((openEditState) => {
-                                    if (openEditState) {
-                                        ctx.addFavorite.editTrack = true;
-                                        ctx.setAddFavorite({ ...ctx.addFavorite });
-                                        setEditWptDialogOpen(true);
-                                    }
-                                });
-                            } else {
-                                setEditWptDialogOpen(true);
-                            }
-                        }}
-                    >
-                        <EditIcon className={styles.wptActionButtonIcon} />
-                    </IconButton>
+                    <Tooltip title={t('shared_string_edit')} arrow placement="bottom">
+                        <IconButton
+                            className={styles.wptActionsButtons}
+                            onClick={() => {
+                                if (wpt.type.isWpt) {
+                                    TracksManager.handleEditCloudTrack(ctx).then((openEditState) => {
+                                        if (openEditState) {
+                                            ctx.addFavorite.editTrack = true;
+                                            ctx.setAddFavorite({ ...ctx.addFavorite });
+                                            setEditWptDialogOpen(true);
+                                        }
+                                    });
+                                } else {
+                                    setEditWptDialogOpen(true);
+                                }
+                            }}
+                        >
+                            <EditIcon className={styles.wptActionButtonIcon} />
+                        </IconButton>
+                    </Tooltip>
                 )}
                 {!wpt.type?.isPoi && (
-                    <IconButton
-                        className={styles.wptActionsButtons}
-                        onClick={() => {
-                            if (wpt.type.isWpt) {
-                                TracksManager.handleEditCloudTrack(ctx).then((openEditState) => {
-                                    if (openEditState) {
-                                        ctx.addFavorite.editTrack = true;
-                                        ctx.setAddFavorite({ ...ctx.addFavorite });
-                                        setDeleteWptDialogOpen(true);
-                                    }
-                                });
-                            } else {
-                                setDeleteWptDialogOpen(true);
-                            }
-                        }}
-                    >
-                        <DeleteIcon className={styles.wptActionButtonIcon} />
-                    </IconButton>
+                    <Tooltip title={t('shared_string_delete')} arrow placement="bottom">
+                        <IconButton
+                            className={styles.wptActionsButtons}
+                            onClick={() => {
+                                if (wpt.type.isWpt) {
+                                    TracksManager.handleEditCloudTrack(ctx).then((openEditState) => {
+                                        if (openEditState) {
+                                            ctx.addFavorite.editTrack = true;
+                                            ctx.setAddFavorite({ ...ctx.addFavorite });
+                                            setDeleteWptDialogOpen(true);
+                                        }
+                                    });
+                                } else {
+                                    setDeleteWptDialogOpen(true);
+                                }
+                            }}
+                        >
+                            <DeleteIcon className={styles.wptActionButtonIcon} />
+                        </IconButton>
+                    </Tooltip>
                 )}
             </Box>
             {editWptDialogOpen && (
