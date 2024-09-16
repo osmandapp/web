b28c00160919ef270d29a0ace3373af26cfb4aab
alisa911 <alisa.91150@gmail.com>
2024-09-16 09:56:47 +0300

    try fix test 84

--- tests/selenium/src/tests/84-logout-by-server.mjs
+++ tests/selenium/src/tests/84-logout-by-server.mjs
@@ -2,7 +2,7 @@ import actionOpenMap from '../actions/actionOpenMap.mjs';
 import actionLogIn from '../actions/actionLogIn.mjs';
 import actionFinish from '../actions/actionFinish.mjs';
 import { driver } from '../options.mjs';
-import { clickBy, waitBy } from '../lib.mjs';
+import { clickBy, waitBy, waitByRemoved } from '../lib.mjs';
 import { By } from 'selenium-webdriver';
 
 export default async function test() {
@@ -12,6 +12,8 @@ export default async function test() {
     await waitBy(By.id('se-logout-button'));
 
     await driver.manage().deleteAllCookies();
+
+    await waitByRemoved(By.id('se-logout-button'));
 
     await waitBy(By.id('se-login-button'));
     await clickBy(By.id('se-login-button'));
