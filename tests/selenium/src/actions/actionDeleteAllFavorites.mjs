c88fc395d06c0d0dbebcc059ef96f6eec79eb56a
alisa911 <alisa.91150@gmail.com>
2024-11-29 12:29:27 +0200

    fix test

--- tests/selenium/src/actions/actionDeleteAllFavorites.mjs
+++ tests/selenium/src/actions/actionDeleteAllFavorites.mjs
@@ -7,6 +7,7 @@ const DEFAULT_FAV_GROUP = 'favorites';
 export default async function test(favorites) {
     let prevName = '';
     let exist;
+    const suffix = '-renamed';
     for (const f of favorites) {
         if (prevName && prevName !== '') {
             await waitByRemoved(By.id(`se-menu-fav-${prevName}`));
@@ -15,6 +16,12 @@ export default async function test(favorites) {
         exist = await waitBy(By.id(`se-menu-fav-${shortFavGroupName}`), { optional: true, idle: true });
         if (exist) {
             await deleteFavGroup(shortFavGroupName);
+        }
+        exist = await waitBy(By.id(`se-menu-fav-${shortFavGroupName}${suffix}`), { optional: true, idle: true });
+        if (exist) {
+            await deleteFavGroup(`${shortFavGroupName}${suffix}`);
+        }
+        if (exist) {
             prevName = shortFavGroupName;
         }
     }
