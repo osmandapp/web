7fd16e2faf87d8f0233b013f70037f2175a4a9f9
alisa911 <alisa.91150@gmail.com>
2025-01-31 13:42:31 +0200

    add stat default values

--- map/src/menu/analyzer/TrackSegmentStat.jsx
+++ map/src/menu/analyzer/TrackSegmentStat.jsx
@@ -24,55 +24,30 @@ import { MAIN_BLOCK_SIZE } from './TrackAnalyzerMenu';
 import { useTranslation } from 'react-i18next';
 
 export const getSpeedStats = (stats, t) => [
-    { icon: <MaxSpeedIcon />, label: t('shared_string_max_speed'), value: stats.maxSpeed?.toFixed(1), unit: t('km_h') },
-    { icon: <AvgSpeedIcon />, label: t('web:avg_speed'), value: stats.avgSpeed?.toFixed(1), unit: t('km_h') },
-    { icon: <MinSpeedIcon />, label: t('shared_string_min_speed'), value: stats.minSpeed, unit: t('km_h') },
+    { icon: <MaxSpeedIcon />, label: t('shared_string_max_speed'), ...formatValue(stats.maxSpeed, t('km_h')) },
+    { icon: <AvgSpeedIcon />, label: t('web:avg_speed'), ...formatValue(stats.avgSpeed, t('km_h')) },
+    { icon: <MinSpeedIcon />, label: t('shared_string_min_speed'), ...formatValue(stats.minSpeed, t('km_h')) },
 ];
 
 export const getAltitudeStats = (stats, t) => [
-    { icon: <MaxAltitudeIcon />, label: t('web:max_altitude'), value: stats.maxElevation, unit: t('m') },
-    {
-        icon: <AvgAltitudeIcon />,
-        label: t('web:avg_altitude'),
-        value: stats.avgElevation?.toFixed(1),
-        unit: t('m'),
-    },
-    { icon: <MinAltitudeIcon />, label: t('web:min_altitude'), value: stats.minElevation, unit: t('m') },
-    {
-        icon: <UphillIcon />,
-        label: t('shared_string_uphill'),
-        value: stats.diffElevationUp?.toFixed(1),
-        unit: t('m'),
-    },
-    {
-        icon: <DownhillIcon />,
-        label: t('shared_string_downhill'),
-        value: stats.diffElevationDown?.toFixed(1),
-        unit: t('m'),
-    },
+    { icon: <MaxAltitudeIcon />, label: t('web:max_altitude'), ...formatValue(stats.maxElevation, t('m')) },
+    { icon: <AvgAltitudeIcon />, label: t('web:avg_altitude'), ...formatValue(stats.avgElevation, t('m')) },
+    { icon: <MinAltitudeIcon />, label: t('web:min_altitude'), ...formatValue(stats.minElevation, t('m')) },
+    { icon: <UphillIcon />, label: t('shared_string_uphill'), ...formatValue(stats.diffElevationUp, t('m')) },
+    { icon: <DownhillIcon />, label: t('shared_string_downhill'), ...formatValue(stats.diffElevationDown, t('m')) },
 ];
 
 export const getOtherStats = (stats, t, formatDate) => [
     { icon: <DateIcon />, label: t('shared_string_date'), value: formatDate(stats.date) },
-    {
-        icon: <TimeDurationIcon />,
-        label: t('duration'),
-        value: (stats.duration / 3600000)?.toFixed(2),
-        unit: t('web:h'),
-    },
-    {
-        icon: <TimeMovingIcon />,
-        label: t('web:moving_time'),
-        value: (stats.timeMoving / 3600000)?.toFixed(2),
-        unit: t('web:h'),
-    },
-    {
-        icon: <DistanceIcon />,
-        label: t('web:length'),
-        value: (stats.totalDist / 1000)?.toFixed(2),
-        unit: t('km'),
-    },
+    { icon: <TimeDurationIcon />, label: t('duration'), ...formatValue(stats.duration, t('web:h'), 3600000) },
+    { icon: <TimeMovingIcon />, label: t('web:moving_time'), ...formatValue(stats.timeMoving, t('web:h'), 3600000) },
+    { icon: <DistanceIcon />, label: t('web:length'), ...formatValue(stats.totalDist, t('km'), 1000) },
 ];
+
+const formatValue = (value, unit = '', factor = 1) => {
+    if (value === 'NaN') return { value: 'NaN', unit: '' };
+    return { value: (Number(value) / factor).toFixed(2), unit };
+};
 
 export default function TrackSegmentStat({ height, sortedSegments, activeSegmentParams }) {
     const { t } = useTranslation();
