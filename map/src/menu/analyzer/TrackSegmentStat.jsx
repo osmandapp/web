31a4516d91446b1b095f37b8da3c2d707bca9b65
alisa911 <alisa.91150@gmail.com>
2025-02-03 12:45:16 +0200

    add expand statistics

--- map/src/menu/analyzer/TrackSegmentStat.jsx
+++ map/src/menu/analyzer/TrackSegmentStat.jsx
@@ -91,53 +91,6 @@ export default function TrackSegmentStat({ height, sortedSegments, activeSegment
         );
     }, [sortedSegments, activeSegmentParams]);
 
-    const Statistics = (stats) => {
-        if (!stats) return null;
-
-        const StatItems = (items, isLastGroup) => (
-            <Box>
-                {items.map((item, index) => (
-                    <Box key={index}>
-                        <MenuItem className={styles.statItem}>
-                            <ListItemIcon className={styles.icon}>{item.icon}</ListItemIcon>
-                            <ListItemText
-                                primary={
-                                    <Box display="flex" justifyContent="space-between" alignItems="center">
-                                        <Typography variant="inherit" noWrap>
-                                            {item.label}
-                                        </Typography>
-                                        <Box display="flex" justifyContent="flex-end" alignItems="center">
-                                            <Typography
-                                                className={styles.statItemValue}
-                                                noWrap
-                                                sx={{ ...(!item.unit && { mr: -1 }) }}
-                                            >
-                                                {item.value}
-                                            </Typography>
-                                            <Typography className={styles.statItemValue2} noWrap sx={{ mt: '2px' }}>
-                                                &nbsp;{item.unit}
-                                            </Typography>
-                                        </Box>
-                                    </Box>
-                                }
-                            />
-                        </MenuItem>
-                        {index < items.length - 1 && <DividerWithMargin dashed={true} />}
-                    </Box>
-                ))}
-                {stats.isLastGroup && !isLastGroup && items.length !== 0 && <SimpleDivider />}
-            </Box>
-        );
-
-        return (
-            <Box>
-                {StatItems(stats.speed, stats.isLastGroup === 'speed')}
-                {StatItems(stats.altitude, stats.isLastGroup === 'altitude')}
-                {StatItems(stats.other, stats.isLastGroup === 'other')}
-            </Box>
-        );
-    };
-
     return useMemo(
         () => (
             <Box sx={{ maxHeight: `${height - MAIN_BLOCK_SIZE}px`, overflowY: 'auto' }}>
@@ -149,7 +102,6 @@ export default function TrackSegmentStat({ height, sortedSegments, activeSegment
                             filteredStats={filteredStats}
                             setFilteredStats={setFilteredStats}
                         />
-                        {Statistics(segment.stats)}
                     </Box>
                 ))}
             </Box>
