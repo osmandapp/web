abfdf1f148d55af02f2d569eb0b59e8007380977
vshcherb <victor.shcherb@gmail.com>
2025-05-16 18:24:16 +0300

    Update ui

--- main/src/components/home/Layout1Block.js
+++ main/src/components/home/Layout1Block.js
@@ -6,9 +6,9 @@ export default function Layout1Block({ children, names, altSectionClass }) {
         <section className={clsx('section', altSectionClass)}>
             <div className="container">
                 <div className="row padding-vert--md">
-                    <div className="col col--2" />
+                    <div className="col col--4" />
 
-                    <div className="col col--6 padding-vert--md padding-horiz--md" align="center">
+                    <div className="col col--4 padding-vert--md padding-horiz--md" align="center">
                         <h1 className="index-title text--center">{names ? names[0] : ''}</h1>
                         {children}
                     </div>
