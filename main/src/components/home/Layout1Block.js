af497a8a63cf7db69db8539f8018a1ac2bf847a5
vshcherb <victor.shcherb@gmail.com>
2025-05-16 14:10:22 +0300

    Remove twitter

---
+++ main/src/components/home/Layout1Block.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import clsx from 'clsx';
+
+export default function Layout1Block({ children, names, altSectionClass }) {
+    return (
+        <section className={clsx('section', altSectionClass)}>
+            <div className="container">
+                <div className="row padding-vert--md">
+                    <div className="col col--2" />
+
+                    <div className="col col--6 padding-vert--md padding-horiz--md" align="center">
+                        <h1 className="index-title text--center">{names ? names[0] : ''}</h1>
+                        {children[0]}
+                    </div>
+                    <div className="col col--4" />
+                </div>
+            </div>
+        </section>
+    );
+}
