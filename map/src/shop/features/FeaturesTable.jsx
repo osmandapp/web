4f38749d4db30ef5cd87ff66740072c8af6026d9
Kseniia Velychko <plotva@MacBook-Pro-Kseniia.local>
2025-05-30 11:45:09 +0300

    fix pricing styles

--- map/src/shop/features/FeaturesTable.jsx
+++ map/src/shop/features/FeaturesTable.jsx
@@ -1,10 +1,12 @@
 import { Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material';
 import { features, planFeatures } from './FeaturesManager';
 import { ReactComponent as CheckIcon } from '../../assets/icons/ic_action_checkmark_colored_day.svg';
+import { ReactComponent as ItemNotIncludedIcon } from '../../assets/icons/ic_action_item_not_included.svg';
 import React from 'react';
 import styles from '../shop.module.css';
 import { useTranslation } from 'react-i18next';
 import { products } from '../products/ProductManager';
+import DividerWithMargin from '../../frame/components/dividers/DividerWithMargin';
 
 const grouped = features.reduce((acc, feat) => {
     (acc[feat.category] = acc[feat.category] || []).push(feat);
@@ -17,11 +19,11 @@ export default function FeaturesTable() {
     const plans = products.filter((p) => p.show).map((p) => ({ key: p.id, label: t(p.name) }));
 
     return (
-        <TableContainer component={Paper} className={styles.pricingTableContainer}>
+        <TableContainer className={styles.pricingTableContainer}>
             <Table className={styles.pricingTable}>
                 <TableHead>
                     <TableRow>
-                        <TableCell />
+                        <TableCell>{'Features'}</TableCell>
                         {plans.map((plan) => (
                             <TableCell key={plan.key} className={styles.planCell}>
                                 {plan.label}
@@ -32,30 +34,37 @@ export default function FeaturesTable() {
                 <TableBody>
                     {Object.entries(grouped).map(([category, feats]) => (
                         <React.Fragment key={category}>
-                            <TableRow className="categoryRow">
-                                <TableCell className="categoryCell" colSpan={plans.length + 1}>
-                                    {category}
-                                </TableCell>
+                            <TableRow className={styles.categoryRow}>
+                                {category !== 'Features' && (
+                                    <TableCell className={styles.categoryCell} colSpan={plans.length + 1}>
+                                        {category}
+                                    </TableCell>
+                                )}
                             </TableRow>
                             {feats.map((feat) => (
-                                <TableRow key={feat.id} className="featureRow">
-                                    <TableCell className={styles.featureCell}>
-                                        <div className={styles.featureWrapper}>
-                                            <div className={styles.featureIcon}>{feat.icon}</div>
-                                            <div className={styles.featureContent}>
-                                                <div className={styles.featureName}>{t(feat.name)}</div>
-                                                <div className={styles.featureDesc}>{t(feat.desc)}</div>
+                                <>
+                                    <TableRow key={feat.id} className={styles.featureRow}>
+                                        <TableCell className={styles.featureCell}>
+                                            <div className={styles.featureWrapper}>
+                                                <div className={styles.featureIcon}>{feat.icon}</div>
+                                                <div className={styles.featureContent}>
+                                                    <div className={styles.featureName}>{t(feat.name)}</div>
+                                                    <div className={styles.featureDesc}>{t(feat.desc)}</div>
+                                                </div>
                                             </div>
-                                        </div>
-                                    </TableCell>
-                                    {plans.map((plan) => (
-                                        <TableCell key={plan.key} className={styles.planCell}>
-                                            {planFeatures[plan.key].includes(feat.id) && (
-                                                <CheckIcon className={styles.checkIcon} />
-                                            )}
                                         </TableCell>
-                                    ))}
-                                </TableRow>
+                                        {plans.map((plan) => (
+                                            <TableCell key={plan.key} className={styles.planCell}>
+                                                {planFeatures[plan.key].includes(feat.id) ? (
+                                                    <CheckIcon className={styles.checkIcon} />
+                                                ) : (
+                                                    <ItemNotIncludedIcon className={styles.checkIcon} />
+                                                )}
+                                            </TableCell>
+                                        ))}
+                                    </TableRow>
+                                    <DividerWithMargin margin={'60px'} />
+                                </>
                             ))}
                         </React.Fragment>
                     ))}
