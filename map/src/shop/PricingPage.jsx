3b2fd9b791ea82c68dbf9b5c9d25e32d27a33a27
Kseniia Velychko <plotva@MacBook-Pro-Kseniia.local>
2025-06-09 16:57:39 +0300

    fix getAccountInfo without login

--- map/src/shop/PricingPage.jsx
+++ map/src/shop/PricingPage.jsx
@@ -7,7 +7,7 @@ import { useTranslation } from 'react-i18next';
 import { purchase } from './products/ProductManager';
 import EmptyLoginDialog from '../login/dialogs/EmptyLoginDialog';
 import { updatePrices } from '../login/fs/FastSpringHelper';
-import { getAccountInfo } from '../manager/LoginManager';
+import { getAccountInfo, INIT_LOGIN_STATE } from '../manager/LoginManager';
 import LoginContext from '../context/LoginContext';
 
 const FeaturesTable = React.lazy(() => import('./features/FeaturesTable'));
@@ -33,13 +33,18 @@ export default function PricingPage() {
     };
 
     useEffect(() => {
-        getAccountInfo(ltx.setAccountInfo).then((info) => {
-            const subscriptions = info?.subscriptions && JSON.parse(info.subscriptions);
-            const inAppPurchases = info?.inAppPurchases && JSON.parse(info.inAppPurchases);
-            setCurrentPurchases({ subscriptions, inAppPurchases });
+        if (ltx.loginUser && ltx.loginUser !== INIT_LOGIN_STATE) {
+            getAccountInfo(ltx.setAccountInfo).then((info) => {
+                const subscriptions = info?.subscriptions && JSON.parse(info.subscriptions);
+                const inAppPurchases = info?.inAppPurchases && JSON.parse(info.inAppPurchases);
+                setCurrentPurchases({ subscriptions, inAppPurchases });
+                updatePrices(setPurchasePriceMap, useTestMode);
+            });
+        } else {
+            setCurrentPurchases({ subscriptions: [], inAppPurchases: [] });
             updatePrices(setPurchasePriceMap, useTestMode);
-        });
-    }, [useTestMode]);
+        }
+    }, [useTestMode, ltx.loginUser]);
 
     useEffect(() => {
         if (purchasePriceMap && Object.keys(purchasePriceMap).length > 0 && currentPurchases) {
