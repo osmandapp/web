7e30707e59e8567b28eb54f01291783593d99649
Kseniia Velychko <plotva@MacBook-Pro-Kseniia.local>
2025-07-01 11:41:29 +0200

    add translations

--- map/src/login/purchases/PurchaseManager.jsx
+++ map/src/login/purchases/PurchaseManager.jsx
@@ -8,6 +8,7 @@ import { ReactComponent as HuaweiIcon } from '../../assets/icons/ic_logo_huawei_
 import { ReactComponent as FastSpringIcon } from '../../assets/icons/ic_action_purchases_outlined.svg';
 import styles from '../../frame/components/items/items.module.css';
 import DividerWithMargin from '../../frame/components/dividers/DividerWithMargin';
+import { sub_state_translate_map } from '../LoginMenu';
 
 const storeMap = {
     'Google Play': {
@@ -32,11 +33,11 @@ const storeMap = {
     },
 };
 
-export function getStatus(state) {
+export function getStatus(state, t) {
     if (state === 'cancelled' || state === 'in_grace_period') {
-        return 'active';
+        state = 'active';
     }
-    return state;
+    return sub_state_translate_map(t)[state];
 }
 
 export default function PurchaseManager({ store }) {
