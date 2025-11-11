2a16b353194ebce826d209ca59f11e85c64eb05c
xmd5a <lornaderek@mail.ru>
2025-11-11 12:09:48 +0300

    Add hazard to legend (https://github.com/osmandapp/OsmAnd/issues/23216)

--- main/src/components/docs/autogenerated/LegendItemAutogeneratedFinance.js
+++ main/src/components/docs/autogenerated/LegendItemAutogeneratedFinance.js
@@ -10,7 +10,7 @@ export default function Render() {
         'Payment terminal' : {poiName:'payment_terminal', poiName2:null, folderName:'finance/finance_amenity_payment_terminal'},
         'Payment centre' : {poiName:'payment_centre', poiName2:null, folderName:'finance/finance_amenity_payment_centre'},
         'Money lender' : {poiName:'money_lender', poiName2:null, folderName:'finance/finance_shop_money_lender'},
-        'Money transfer' : {poiName:'null', poiName2:null, folderName:'finance/finance_amenity_money_transfer'},
+        'Money transfer' : {poiName:'money_transfer', poiName2:null, folderName:'finance/finance_amenity_money_transfer'},
         'Bureau de change' : {poiName:'bureau_de_change', poiName2:null, folderName:'finance/finance_amenity_bureau_de_change'},
         'Mobile money agent' : {poiName:'mobile_money_agent', poiName2:null, folderName:'finance/finance_amenity_mobile_money_agent'},
         'Ticket validator' : {poiName:'ticket_validator', poiName2:null, folderName:'finance/finance_amenity_ticket_validator'},
