88aabfa2abd90953f812cdf24fe7af3458e8d63a
Kseniia Velychko <alisa.91150@gmail.com>
2025-12-09 12:40:00 +0200

    fix freezes with map

--- map/src/map/util/Clusterizer.js
+++ map/src/map/util/Clusterizer.js
@@ -378,9 +378,12 @@ export function createHoverMarker({
     let tooltipRef = ctx.searchTooltipRef;
     let pointerRef = ctx.searchPointerRef;
 
-    const onZoomEnd = () => {
-        removeTooltip(map, tooltipRef);
-    };
+    if (!map._sharedZoomEndHandler) {
+        map._sharedZoomEndHandler = () => {
+            removeTooltip(map, tooltipRef);
+        };
+        map.on('zoomend', map._sharedZoomEndHandler);
+    }
 
     const onMouseOver = () => {
         removeTooltip(map, tooltipRef);
@@ -449,13 +452,11 @@ export function createHoverMarker({
         }
     };
 
-    map.on('zoomend', onZoomEnd);
     marker.on('mouseover', onMouseOver);
     marker.on('mouseout', onMouseOut);
     marker.on('selectMarker', onSelectMarker);
 
     return () => {
-        map.off('zoomend', onZoomEnd);
         marker.off('mouseover', onMouseOver);
         marker.off('mouseout', onMouseOut);
         marker.off('selectMarker', onSelectMarker);
