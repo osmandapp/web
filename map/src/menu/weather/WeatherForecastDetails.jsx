177f563cee16e4965a522b13fd13fb2963aa413d
Kseniia Velychko <alisa.91150@gmail.com>
2026-01-05 12:54:54 +0200

    fix weather test

--- map/src/menu/weather/WeatherForecastDetails.jsx
+++ map/src/menu/weather/WeatherForecastDetails.jsx
@@ -170,22 +170,12 @@ export default function WeatherForecastDetails({ setShowInfoBlock }) {
     function getSavedForecast() {
         if (localStorage.getItem(LOCAL_STORAGE_WEATHER_FORECAST_WEEK)) {
             const weatherData = JSON.parse(localStorage.getItem(LOCAL_STORAGE_WEATHER_FORECAST_WEEK));
-
-            // Check if the data is an array, not empty, contains only arrays, and has more than one element
             if (
                 Array.isArray(weatherData) &&
                 weatherData.length > 0 &&
-                weatherData.every(Array.isArray) &&
-                weatherData.length > 1
+                weatherData.every((item) => typeof item === 'object' && item !== null && 'ts' in item)
             ) {
-                // Check if the first inner array contains exactly 8 elements
-                if (weatherData[0].length === 8) {
-                    return weatherData;
-                } else {
-                    console.debug('The first array does not contain 8 elements.');
-                }
-            } else {
-                console.debug('The data does not meet the requirements.');
+                return weatherData;
             }
         } else {
             console.debug('No data in local storage.');
