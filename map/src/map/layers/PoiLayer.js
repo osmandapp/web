3bb1271a6887bbf011a6c3771276ca046974f224
Kseniia Velychko <alisa.91150@gmail.com>
2026-03-17 17:40:34 +0200

    fix poi clusters if alreadyFound

--- map/src/map/layers/PoiLayer.js
+++ map/src/map/layers/PoiLayer.js
@@ -474,10 +474,21 @@ export default function PoiLayer() {
                     const res = await getPoi(controller, showPoiCategories, visibleBboxInfo, savedBbox, prevCategories);
                     if (reqId !== reqIdRef.current || ignore) return;
                     if (res) {
-                        if (!res.alreadyFound && res.features) {
+                        let features = null;
+                        let listFeatures = null;
+
+                        if (res.features?.features?.length > 0) {
+                            features = res.features.features;
+                            listFeatures = res.features;
+                        } else if (res.alreadyFound && poiList?.listFeatures?.features?.length > 0) {
+                            features = poiList.listFeatures.features;
+                            listFeatures = poiList.listFeatures;
+                        }
+
+                        if (features?.length && listFeatures) {
                             const layer = await createPoiLayer({
                                 ctx,
-                                poiList: res.features.features,
+                                poiList: features,
                                 globalPoiIconCache: poiIconCache,
                                 map,
                                 zoom,
@@ -486,15 +497,14 @@ export default function PoiLayer() {
                             if (existing) {
                                 map.removeLayer(existing);
                             }
-                            const newPoiList = {
+                            setPoiList({
                                 prevLayer: cloneDeep(poiList?.layer),
                                 layer,
-                                listFeatures: res.features,
-                            };
-                            setPoiList(newPoiList);
+                                listFeatures,
+                            });
                             setBbox(bbox);
                             setPrevCategories(showPoiCategories);
-                            setUseLimit(res.useLimit);
+                            setUseLimit(res.useLimit ?? false);
                         }
                     } else {
                         setPoiList(null);
