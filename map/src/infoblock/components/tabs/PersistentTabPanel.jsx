9da17df28e970db57a19e3119949473fe6163765
Kseniia Velychko <alisa.91150@gmail.com>
2026-03-20 11:15:36 +0200

    move setState from render to useEffect

--- map/src/infoblock/components/tabs/PersistentTabPanel.jsx
+++ map/src/infoblock/components/tabs/PersistentTabPanel.jsx
@@ -1,18 +1,24 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Box, Typography } from '@mui/material';
 
 export default function PersistentTabPanel({ tabId, selectedTabId, children }) {
     const [mounted, setMounted] = useState(false);
 
-    if (tabId === selectedTabId || mounted) {
-        mounted || setMounted(true);
-        const hidden = tabId !== selectedTabId;
-        return (
-            <Typography hidden={hidden} component="span">
-                <Box sx={{ px: 3, pt: 3, pb: 8 }}>{children}</Box>
-            </Typography>
-        );
+    useEffect(() => {
+        if (tabId === selectedTabId && !mounted) {
+            setMounted(true);
+        }
+    }, [tabId, selectedTabId]);
+
+    if (!mounted && tabId !== selectedTabId) {
+        return null;
     }
 
-    return null;
+    const hidden = tabId !== selectedTabId;
+
+    return (
+        <Typography hidden={hidden} component="span">
+            <Box sx={{ px: 3, pt: 3, pb: 8 }}>{children}</Box>
+        </Typography>
+    );
 }
