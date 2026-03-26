a19762021767bce938f57f8849926cb29aa20a65
Dima-1 <softmail32@gmail.com>
2026-03-26 11:03:48 +0200

    Fix 9 test

--- tests/selenium/src/tests/tracks/09-smart-folders-loading.mjs
+++ tests/selenium/src/tests/tracks/09-smart-folders-loading.mjs
@@ -5,21 +5,22 @@ import actionOpenMap from '../../actions/map/actionOpenMap.mjs';
 import actionLogIn from '../../actions/login/actionLogIn.mjs';
 import actionFinish from '../../actions/actionFinish.mjs';
 import actionIdleWait from '../../actions/actionIdleWait.mjs';
-import { TEST_LOGIN3, TEST_PASSWORD3 } from '../../options.mjs';
+import { TEST_LOGIN2, TEST_PASSWORD2 } from '../../options.mjs';
 
 const SMART_FOLDER_NAME = 'sf5len2';
 const DELAY_AFTER_REFRESH_MS = 5000;
 
 export default async function test() {
     await actionOpenMap();
-    await actionLogIn({ login: TEST_LOGIN3, password: TEST_PASSWORD3 });
+    await actionLogIn({ login: TEST_LOGIN2, password: TEST_PASSWORD2 });
     await clickBy(By.id('se-show-menu-tracks'));
     await driver.navigate().refresh();
     await waitByRemoved(By.id('se-loading-page'), true);
     const loadedInDelay = await driver
-        .wait(async () => {
-            return await waitBy(By.id(`se-menu-cloud-${SMART_FOLDER_NAME}`));
-        }, DELAY_AFTER_REFRESH_MS)
+    .wait(async () => {
+        const found = await driver.findElements(By.id(`se-menu-cloud-${SMART_FOLDER_NAME}`));
+        return found.length > 0;
+    }, DELAY_AFTER_REFRESH_MS)
         .catch(() => false);
     await assert(loadedInDelay, `Smart folders did not load within ${DELAY_AFTER_REFRESH_MS/1000} seconds after page refresh`);
     await waitByRemoved(By.id('se-loading-page'), true);
