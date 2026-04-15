0430be0bf41c0a9c2c477c2f7f39f0842717faa1
Kseniia Velychko <alisa.91150@gmail.com>
2026-04-15 11:17:58 +0300

    add verions for indexdb

--- map/src/index.js
+++ map/src/index.js
@@ -3,10 +3,16 @@ import './assets/css/index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { createRoot } from 'react-dom/client';
+import { ensureAppDataVersion } from './util/appDataVersion';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 
+try {
+    await ensureAppDataVersion();
+} catch (e) {
+    console.error(e);
+}
 root.render(<App />);
 
 // If you want to start measuring performance in your app, pass a function
