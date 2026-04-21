523305e9d4fc79fe50df61d48907305abbc2b39f
Kseniia Velychko <alisa.91150@gmail.com>
2026-04-21 07:01:12 +0300

    update garmin group

--- map/src/login/garmin/GarminConnectedView.jsx
+++ map/src/login/garmin/GarminConnectedView.jsx
@@ -14,7 +14,7 @@ import { fmt } from '../../util/dateFmt';
 import { EMPTY_FILE_NAME } from '../../manager/track/TracksManager';
 import AppContext from '../../context/AppContext';
 
-const GARMIN_FOLDER_NAME = 'Garmin';
+export const GARMIN_FOLDER_NAME = 'Garmin Connect';
 
 export default function GarminConnectedView({ syncTimeMs, disconnecting, onDisconnectClick }) {
     const ctx = useContext(AppContext);
