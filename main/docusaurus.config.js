e8e046f4c289afec237a5f24541142e0fbc50c85
Eugene <prozmeu@gmail.com>
2026-06-18 00:12:21 +0200

    removed sale banner

--- main/docusaurus.config.js
+++ main/docusaurus.config.js
@@ -137,7 +137,7 @@ async function createConfig(){
               label: 'Docs',
             },
             { to: '/blog', label: 'Blog', position: 'left'},
-            { to: 'pathname:///pricing', label: '🏖️ Sale & Pricing', position: 'left' },
+            { to: 'pathname:///pricing', label: '💳 Pricing', position: 'left' },
             { to: 'pathname:///map', target: '_self', label: '🌍 Map', position: 'left' },
             { to: '/docs/hiring', label: '🚵‍♂️ Join us', position: 'left' },
             {
@@ -155,7 +155,7 @@ async function createConfig(){
               title: 'OsmAnd',
               items: [
                 {
-                  label: 'Pricing & Sale🏖️ ',
+                  label: 'Pricing 💳',
                   to: 'pathname:///pricing',
                 },
                 {
