9553985aedf37471860a6885d3c42a61679f9a20
RZR-UA <youonly@ukr.net>
2026-06-25 16:42:15 +0200

    Hotfix MVT_OSMAND_URL

--- map/src/map/mvt/MvtDemoConfig.js
+++ map/src/map/mvt/MvtDemoConfig.js
@@ -2,8 +2,11 @@ export const MVT_OSMAND_LAYER = 'mvt-osmand';
 
 // URL in defaultStyle.json is used for external style editors only
 // This URL overrides the default one and requires setupProxy.js to proxy /vector/ queries
-const port = window.location.port === 80 || window.location.port === 443 ? '' : `:${window.location.port}`;
-export const MVT_OSMAND_URL = `${window.location.protocol}//${window.location.hostname}${port}/vector/{z}/{x}/{y}.mvt`;
+const port = window.location.port;
+const host = window.location.host.toLowerCase();
+const tilesPort = !port || port === 80 || port === 443 ? '' : `:${port}`;
+const tilesHost = host === 'osmand.net' || host === 'www.osmand.net' ? 'maptile.osmand.net' : host;
+export const MVT_OSMAND_URL = `${window.location.protocol}//${tilesHost}${tilesPort}/vector/{z}/{x}/{y}.mvt`;
 
 export const MVT_OSM_TEST_LAYER = 'mvt-osm-test';
 export const MVT_OSM_TEST_URL = 'https://tiles.openstreetmap.us/vector/openmaptiles/{z}/{x}/{y}.mvt'; // mvtOsmStyle.json
