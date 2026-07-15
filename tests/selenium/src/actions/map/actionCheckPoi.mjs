c3796a8f4517d7a32153b18aebd5fd086b2dc994
RZR-UA <youonly@ukr.net>
2026-07-15 14:08:40 +0200

    Stabilize actionCheckPoi with 2-way waitByPoiMarker

--- tests/selenium/src/actions/map/actionCheckPoi.mjs
+++ tests/selenium/src/actions/map/actionCheckPoi.mjs
@@ -1,5 +1,25 @@
-import { waitBy, waitByRemoved } from '../../lib.mjs';
+import { enclose, waitBy, waitByRemoved } from '../../lib.mjs';
 import { By } from 'selenium-webdriver';
+import { driver } from '../../options.mjs';
+
+async function waitByPoiMarker({ iconWpt, colorIcon, name }) {
+    const icon = By.id(`se-poi-marker-icon-${iconWpt}-${colorIcon}-${name}`);
+    const marker = By.css(`[se-poi-name="${name}"]`);
+    return await enclose(
+        async () => {
+            const icons = await driver.findElements(icon);
+            if (icons?.length > 0) {
+                return icons[0];
+            }
+            const markers = await driver.findElements(marker);
+            if (markers?.length > 0) {
+                return markers[0];
+            }
+            return false;
+        },
+        { tag: `waitByPoiMarker (${name})` }
+    );
+}
 
 export default async function test({
     iconWpt = null,
@@ -22,7 +42,7 @@ export default async function test({
     } else {
         await waitBy(By.id(`se-poi-marker-background-${colorShape}-${shape}`));
         if (iconWpt) {
-            await waitBy(By.id(`se-poi-marker-icon-${iconWpt}-${colorIcon}-${name}`));
+            await waitByPoiMarker({ iconWpt, colorIcon, name });
         }
         if (isSelected) {
             const colorHex = colorShape.replace(/^#/, '');
