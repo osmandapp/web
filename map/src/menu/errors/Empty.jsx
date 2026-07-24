b41d432d31e1c9ca45b0961ae51d656be2881142
Dima-1 <softmail32@gmail.com>
2026-07-24 12:53:07 +0300

    Refactor Empty.jsx

--- map/src/menu/errors/Empty.jsx
+++ map/src/menu/errors/Empty.jsx
@@ -1,5 +1,6 @@
 import { Box, Button, Icon, ListItemText } from '@mui/material';
 import { ReactComponent as EmptyIcon } from '../../assets/icons/ic_action_folder_open.svg';
+import { ReactComponent as EmptyIconCloud } from '../../assets/icons/ic_action_folder_open_cloud_colored.svg';
 import styles from './errors.module.css';
 import CloudGpxUploader from '../../frame/util/CloudGpxUploader';
 import { useContext } from 'react';
@@ -7,29 +8,21 @@ import { OBJECT_TYPE_FAVORITE } from '../../context/AppContext';
 import EmptyLogin from '../../login/EmptyLogin';
 import { FREE_ACCOUNT } from '../../manager/LoginManager';
 import LoginContext from '../../context/LoginContext';
+import { useTranslation } from 'react-i18next';
+import ButtonPro from '../../frame/pro/ButtonPro';
 
 export default function Empty({ title, text, folder = null, menu = null, checkLogin = true }) {
     const ltx = useContext(LoginContext);
+    const { t } = useTranslation();
 
-    function showImportBtn() {
-        if (folder !== null) {
-            if (menu === OBJECT_TYPE_FAVORITE) {
-                return true;
-            } else {
-                if (ltx.accountInfo?.account !== FREE_ACCOUNT) {
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
-
-    function getText() {
-        if (menu !== OBJECT_TYPE_FAVORITE && ltx.accountInfo?.account === FREE_ACCOUNT) {
-            return 'OsmAnd Pro subscription is required to import or create tracks in OsmAnd Cloud.';
-        }
-        return text;
-    }
+    const isFavoriteMenu = menu === OBJECT_TYPE_FAVORITE;
+    const isFreeAccount = ltx.accountInfo?.account === FREE_ACCOUNT;
+    const requiresProForTracks = !isFavoriteMenu && isFreeAccount;
+    const showImportBtn = folder !== null && !requiresProForTracks;
+    const displayText = requiresProForTracks
+        ? t('empty_cloud_tracks_description')
+        : text;
+    const IconComponent = requiresProForTracks ? EmptyIconCloud : EmptyIcon;
 
     function checkLoginUser() {
         if (checkLogin) {
@@ -43,22 +36,26 @@ export default function Empty({ title, text, folder = null, menu = null, checkLo
             {checkLoginUser() ? (
                 <Box className={styles.block} id="se-empty-page">
                     <Icon className={styles.icon}>
-                        <EmptyIcon className={styles.icon} />
+                        <IconComponent className={styles.icon} />
                     </Icon>
                     <Box className={styles.info}>
                         <ListItemText disableTypography={true} className={styles.title}>
                             {title}
                         </ListItemText>
                         <ListItemText disableTypography={true} className={styles.text}>
-                            {getText()}
+                            {displayText}
                         </ListItemText>
                     </Box>
-                    {showImportBtn() && (
-                        <CloudGpxUploader folder={folder} style={styles.label}>
-                            <Button id="se-import-first-track" className={styles.button} component="span">
-                                Import
-                            </Button>
-                        </CloudGpxUploader>
+                    {requiresProForTracks ? (
+                        <ButtonPro />
+                    ) : (
+                        showImportBtn && (
+                            <CloudGpxUploader folder={folder} style={styles.label}>
+                                <Button id="se-import-first-track" className={styles.button} component="span">
+                                    Import
+                                </Button>
+                            </CloudGpxUploader>
+                        )
                     )}
                 </Box>
             ) : (
