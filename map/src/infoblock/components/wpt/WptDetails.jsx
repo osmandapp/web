5fe6d38480995c3d4aa0dc70facbc216110c9d50
Kseniia Velychko <alisa.91150@gmail.com>
2026-08-24 12:31:09 +0300

    Fix photos lost after tag filter refactoring

--- map/src/infoblock/components/wpt/WptDetails.jsx
+++ map/src/infoblock/components/wpt/WptDetails.jsx
@@ -684,14 +684,14 @@ export default function WptDetails({ setOpenWptTab, setShowInfoBlock }) {
 
     async function getPhotos(wpt) {
         let tags;
-        const objTags = wpt.tags?.res;
+        const rawTags = wpt.tags?.rawTags;
         const wikidataId = wpt.wikidata;
-        if (!objTags && wikidataId) {
+        if (!rawTags && wikidataId) {
             tags = {
                 wikidata: wikidataId,
             };
-        } else {
-            tags = Object.fromEntries(wpt.tags.res.map(({ key, value }) => [key, String(value)]));
+        } else if (rawTags) {
+            tags = Object.fromEntries(Object.entries(rawTags).map(([key, value]) => [key, String(value)]));
             if (!tags[WIKIDATA] && wikidataId) {
                 tags[WIKIDATA] = wikidataId;
             }
